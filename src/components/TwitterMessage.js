99e4512a683cfca10d9d603caf1feaee15893fad
Fay Vera <fvera.cavalcante@gmail.com>
2021-05-17 12:00:14 -0400

    commit tree 7d53bb3d4bb9210361a9e70a4be3c7a20173bc95

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,22 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {message: ''};
+  }
+
+  countChars = (event) => {
+    this.setState({
+      message: event.target.value
+    })
   }
 
   render() {
+    let charNum = this.props.maxChars - this.state.message.length
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" value={this.state.message} onChange={this.countChars} />
+        {charNum}
       </div>
     );
   }
